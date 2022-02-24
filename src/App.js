bffc9b5a4b7eb6d7cd8acc0d84c2f14d2b38bf24
francosilva25 <e.franco250299@gmail.com>
2022-02-24 18:53:58 -0500

    Test write component in app

--- src/App.js
+++ src/App.js
@@ -1,12 +1,13 @@
-// import HomePage from "./pages/home/home";
 import TopBar from "./components/topbar/TopBar"
-import Single from "./pages/single/Single";
+import Write from "./pages/write/Write";
+// import HomePage from "./pages/home/home";
+// import Single from "./pages/single/Single";
 
 function App() {
   return (
     <>
       <TopBar />
-      <Single />
+      <Write />
     </>
   );
 }

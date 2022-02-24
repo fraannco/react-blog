630e3d8eb1e2cd61fdde22ec331a2b3f75dedbf3
francosilva25 <e.franco250299@gmail.com>
2022-02-24 14:47:45 -0500

    Create SinglePost component

--- src/components/singlePost/SinglePost.jsx
+++ src/components/singlePost/SinglePost.jsx
@@ -1,9 +1,18 @@
-import "./singlepost.css"
+import "./singlepost.css";
 
 const SinglePost = () => {
   return (
-    <div className="singlePost">SinglePost</div>
-  )
-}
+    <div className="singlePost">
+      <div className="singlePostWrapper">
+        <img
+          className="singlePostImg"
+          src="https://images.pexels.com/photos/442154/pexels-photo-442154.jpeg?auto=compress&cs=tinysrgb&h=750&w=1260"
+          alt=""
+        />
+        <h1 className="singlePostTitle">Lorem ipsum dolor sit amet</h1>
+      </div>
+    </div>
+  );
+};
 
-export default SinglePost
+export default SinglePost;

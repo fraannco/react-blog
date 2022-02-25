12d03b331c1ad1c9a6fce248d4696e30d26374de
francosilva25 <e.franco250299@gmail.com>
2022-02-24 19:58:25 -0500

    Create routes

--- src/App.js
+++ src/App.js
@@ -3,15 +3,23 @@ import Login from "./pages/login/Login";
 import Register from "./pages/register/Register";
 import Settings from "./pages/settings/Settings";
 import Write from "./pages/write/Write";
-// import HomePage from "./pages/home/home";
-// import Single from "./pages/single/Single";
+import HomePage from "./pages/home/home";
+import Single from "./pages/single/Single";
+import { BrowserRouter, Route, Routes } from "react-router-dom";
 
 function App() {
   return (
-    <>
-      <TopBar />
-      <Register />
-    </>
+    <BrowserRouter>
+    <TopBar />
+      <Routes>
+        <Route exact path="/" element={<HomePage />} />
+        <Route path="/login" element={<Login />} />
+        <Route path="/register" element={<Register />} />
+        <Route path="/settings" element={<Settings />} />
+        <Route path="/write" element={<Write />} />
+        <Route path="/post/:postId" element={<Single />} />
+      </Routes>
+    </BrowserRouter>
   );
 }
 

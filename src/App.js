acf11358cfdf1e9c82e67fc3894ba8a1bb18ab94
francosilva25 <e.franco250299@gmail.com>
2022-02-24 19:30:25 -0500

    Add settings component to app

--- src/App.js
+++ src/App.js
@@ -1,4 +1,5 @@
 import TopBar from "./components/topbar/TopBar"
+import Settings from "./pages/settings/Settings";
 import Write from "./pages/write/Write";
 // import HomePage from "./pages/home/home";
 // import Single from "./pages/single/Single";
@@ -7,7 +8,7 @@ function App() {
   return (
     <>
       <TopBar />
-      <Write />
+      <Settings />
     </>
   );
 }
